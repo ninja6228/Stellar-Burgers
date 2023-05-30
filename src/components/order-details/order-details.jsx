bfa3f8693efb0e729bf2f9b2b8d97bd9c32c773b
Ninja6228 <lesha6228nazarov@gmail.com>
2023-05-30 12:37:05 +0300

    fix: icon format

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import style from './order-details.module.css';
-import checkMarkIcon from "../../images/modal/checkMarkIcon.png";
+import done from "../../images/modal/done.svg";
 
 function OrderDetails({ orderDetails }) {
   return (
@@ -8,7 +8,7 @@ function OrderDetails({ orderDetails }) {
       <div className={`${style.wrapper} mt-30 mb-30`}>
         <h2 className={`${style.number} text text_type_digits-large mb-8`}>{orderDetails}</h2>
         <p className={`text text_type_main-medium mb-15`} >идентификатор заказа</p>
-        <img className={`${style.img} mb-15`} src={checkMarkIcon} alt="иконка галочки" />
+        <img className={`${style.img} mb-15`} src={done} alt="иконка галочки" />
         <p className='text text_type_main-default mb-2'>Ваш заказ начали готовить</p>
         <p className='text text_type_main-default text_color_inactive'>Дождитесь готовности на орбитальной станции</p>
       </div>
